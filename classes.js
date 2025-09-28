0b2bfbc1c9ee458e95724403e3c2a0aac03c675b
jimmmzzzzz <feyijimiosibanjo@gmail.com>
2025-09-28 03:32:29 +0100

    classes.js

--- classes.js
+++ classes.js
@@ -1,11 +1,12 @@
+//classes in javaScript are a blueprint for creating objects.
 class student {
     constructor(name , matric) {
         this.name = name;
         this.matric = matric;
     }
     displayInfo() {
-        consol.log(`Name: ${this.name} , Matric No: ${this.matric}`);
+        consol.log(`Name: ${this.name} , Matric No: ${this.matric}`); 
     }
 }
-const student1 = new Student("jimi","24/2303");
+let student1 = new Student("jimi","24/2303");
 student1.displayInfo();
