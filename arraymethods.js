c699292f1071ce160d223cb99513c131d89dccf7
jimmmzzzzz <feyijimiosibanjo@gmail.com>
2025-09-28 03:30:27 +0100

    arraymethods.js

--- arraymethods.js
+++ arraymethods.js
@@ -1,4 +1,5 @@
-const scores = [45, 65, 38, 66, 90];
-const doubled = scores.map(score => score * 2);
-const passed = scores.filter(score => score >= 50);
-const total = scores.reduce((sum , score) => sum + score, 0);
+//array methods creats a new array by applying a function to each element of an existing array
+let scores = [45, 65, 38, 66, 90];
+let doubled = scores.map(score => score * 2);
+let passed = scores.filter(score => score >= 50);
+let total = scores.reduce((sum , score) => sum + score, 0);
