782a7b0a68e4585c7eed44578fb9a39d35a28cd5
jimmmzzzzz <feyijimiosibanjo@gmail.com>
2025-09-28 03:35:06 +0100

    spreadoperator.js

--- spreadoperator.js
+++ spreadoperator.js
@@ -1,3 +1,4 @@
-const arr1 = [1 , 2 , 3];
-const arr2 = [4 , 5 , 6];
-const merged = [...arr1, ...arr2];
+//spread operator allows you to expand arrays or objects.
+let arr1 = [1 , 2 , 3];
+let arr2 = [4 , 5 , 6];
+let merged = [...arr1, ...arr2];
